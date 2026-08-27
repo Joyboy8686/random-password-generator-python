2937fd8d308b8ff25f8051321c753d72035ba5b3
Hunter <junedbagwan4401@gmail.com>
2026-08-27 22:59:49 +0530

    Fix password length validation error message

--- randompassgene.py
+++ randompassgene.py
@@ -11,12 +11,12 @@ print("\nRANDOM PASSWORD GENERATOR\n\n")
 while True:
 	try:
 		length = int(input("Enter Desired Length For Password : "))
-		if length <= 0 or length > 16:
-			print("Error : Value Must be positive digit and Between 1 to 16!")
+		if length <= 3 or length > 16:
+			print("Error : Value Must be digit Between 4 to 16!")
 		else:
 			break
 	except ValueError:
-		print("Error : Value Must be positive digit!")
+		print("Error : Value Must be digit Between 4 to 16!")
 
 password = "".join(random.choices(mix,k = length))
-print(password)
+print(f"\nPASSWORD : {password}")
